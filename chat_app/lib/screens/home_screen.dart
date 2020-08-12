f31c6407b810646952d8c3db603632a2ac10f4c4
OliverLoenning <Oliver.S.Loenning@gmail.com>
2020-08-12 13:34:26 +0200

    list view done

--- chat_app/lib/screens/home_screen.dart
+++ chat_app/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/classes/firebase_account.dart';
 import 'package:chat_app/interfaces/account.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,19 +8,55 @@ class HomeScreen extends StatelessWidget {
   final Account _account = FirebaseAccount();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Home Page"),
-        ),
-        body: Center(
-          child: RaisedButton(
-            child: Text(
-              "Log out",
+    Row _createListTile(String id, String title) {
+      return Row(
+        children: [
+          Container(
+              margin: EdgeInsets.only(left: 12), child: Icon(Icons.person_pin)),
+          Expanded(
+            child: FlatButton(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  title,
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+              onPressed: () {
+                print(id);
+              },
             ),
-            onPressed: () {
-              _account.logOut(context);
-            },
-          ),
-        ));
+          )
+        ],
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection("users").snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return new Text("Loading...");
+            default:
+              return ListView(
+                children:
+                    snapshot.data.documents.map((DocumentSnapshot document) {
+                  return _createListTile(document["id"], document['email']);
+                }).toList(),
+              );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_back_ios),
+        onPressed: () {
+          _account.logOut(context);
+        },
+      ),
+    );
   }
 }
