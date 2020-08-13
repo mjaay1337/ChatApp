71f2cb43a30edc5df0b22944ecdbe9ea8aee4928
mjaay1337 <mbj3400@gmail.com>
2020-08-13 14:21:51 +0200

    cleaned up files

--- chat_app/lib/screens/forgot_pass.dart
+++ chat_app/lib/screens/forgot_pass.dart
@@ -1,7 +1,6 @@
 import 'package:chat_app/cubit/forgot_pass_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class ForgotPassScreen extends StatelessWidget {
   static const String id = 'forgot_pass';
