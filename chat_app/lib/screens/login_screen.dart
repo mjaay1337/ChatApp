6ebfbb18d3ca8100ed781505325320e573215980
mjaay1337 <mbj3400@gmail.com>
2020-08-13 14:52:36 +0200

    cleaned up some code

--- chat_app/lib/screens/login_screen.dart
+++ chat_app/lib/screens/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:chat_app/cubit/login_cubit.dart';
-import 'package:chat_app/screens/sign_up_screen.dart';
 import 'package:chat_app/widgets/account_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,34 +12,35 @@ class LoginScreen extends StatelessWidget {
         title: Text("Login"),
       ),
       body: Center(
-          child: BlocBuilder<LoginCubit, LoginState>(
-        cubit: BlocProvider.of<LoginCubit>(context),
-        builder: (context, state) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AccountTextField(
-                  obscureText: false,
-                  labelText: 'Email',
-                  onChanged: (value) {
-                    context.bloc<LoginCubit>().changeEmail(value);
-                  }),
-              AccountTextField(
-                  obscureText: true,
-                  labelText: 'Password',
-                  onChanged: (value) {
-                    context.bloc<LoginCubit>().changePassword(value);
-                  }),
-              RaisedButton(
-                onPressed: () {
-                  context.bloc<LoginCubit>().login();
-                },
-                child: Text("Login"),
-              )
-            ],
-          );
-        },
-      )),
+        child: BlocBuilder<LoginCubit, LoginState>(
+          cubit: BlocProvider.of<LoginCubit>(context),
+          builder: (context, state) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AccountTextField(
+                    obscureText: false,
+                    labelText: 'Email',
+                    onChanged: (value) {
+                      context.bloc<LoginCubit>().changeEmail(value);
+                    }),
+                AccountTextField(
+                    obscureText: true,
+                    labelText: 'Password',
+                    onChanged: (value) {
+                      context.bloc<LoginCubit>().changePassword(value);
+                    }),
+                RaisedButton(
+                  onPressed: () {
+                    context.bloc<LoginCubit>().login();
+                  },
+                  child: Text("Login"),
+                )
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
