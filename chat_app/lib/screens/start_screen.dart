9b3a293adc7e2f41700d1853990ab5929ba2d5ed
Oliver <Oliver.S.Loenning@gmail.com>
2020-08-14 09:38:16 +0200

    animated text kit

--- chat_app/lib/screens/start_screen.dart
+++ chat_app/lib/screens/start_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:chat_app/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/screens/sign_up_screen.dart';
@@ -12,6 +13,18 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          SizedBox(
+            width: 250.0,
+            child: TyperAnimatedTextKit(
+              speed: Duration(milliseconds: 2000),
+              text: [
+                "Just Chat",
+              ],
+              textStyle: TextStyle(fontSize: 32.0, fontFamily: "Agne"),
+              textAlign: TextAlign.center,
+              alignment: AlignmentDirectional.topStart, // or Alignment.topLeft
+            ),
+          ),
           RaisedButton(
             color: Colors.white,
             onPressed: () {
